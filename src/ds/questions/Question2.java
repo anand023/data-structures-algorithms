89026e17b5cf54c337d368c658f19406f76b1e2e
anand023 <anandabhi.anand023@gmail.com>
2021-05-15 03:56:20 +0530

    Add files via upload

---
+++ src/ds/questions/Question2.java
@@ -0,0 +1,17 @@
+package ds.questions;
+
+/*
+* Suppose we had an algorithm that took array of strings,
+* sorted each string, and then sorted the full array.
+* what would the runtime be
+* */
+public class Question2 {
+    private String[] sortedStringArray ;
+    public void sortStringThenStore(String[] str){
+        sortedStringArray= new String[str.length];
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
